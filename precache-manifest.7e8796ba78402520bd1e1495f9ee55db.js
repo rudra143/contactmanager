8975bf0f4ecc4e6ac5d11320c22e35407b6f8fb1
Maharudra Kekan <mkekan77@gmail.com>
2020-06-03 16:33:39 +0530

    Updates

---
+++ precache-manifest.7e8796ba78402520bd1e1495f9ee55db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f898504b8eac689411442e7a8aed2f87",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "6cc423ec0dd22617938c",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6cc423ec0dd22617938c",
+    "url": "/contactmanager/static/js/2.d70e0960.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.d70e0960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "694e3dd13bc8184c8345",
+    "url": "/contactmanager/static/js/main.a4ae6a9e.chunk.js"
+  },
+  {
+    "revision": "31aa565394677d9739a4",
+    "url": "/contactmanager/static/js/runtime-main.ab1f59d6.js"
+  }
+]);
